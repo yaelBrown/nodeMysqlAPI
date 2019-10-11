b14397794c0dfc15ad1ab1f3e186064e38d670a1
yaelBrown <yaelrbrown@gmail.com>
2019-10-11 16:02:34 -0500

    Serves index.html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = 3000
+
 
 const mysql = require('mysql');
 
@@ -16,6 +18,6 @@ con.connect(function(err) {
 });
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
