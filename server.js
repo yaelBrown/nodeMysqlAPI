798b9751abcfeb85e950d704ca87baa82e03a38d
yaelBrown <yaelrbrown@gmail.com>
2019-10-11 16:42:32 -0500

    installed fileupload and setup middleware.

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const port = 3000
 
-
+// Connect db
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "codeup"
+  password: "codeup",
+  database: 'employees'
 });
 
-con.connect(function(err) {
+db.connect(function(err) {
   if (err) throw err;
   console.log("MySQL Connected!");
 });
 
-
+// Setup middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
