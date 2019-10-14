7d63e0fef283165bd0db6db8de7006723e6bd652
Yael Brown <yaelrbrown@gmail.com>
2019-10-14 01:48:17 -0500

    Able to GET and POST to mysql db. Added code from lecture.

--- server.js
+++ server.js
@@ -10,14 +10,17 @@ const mysql = require('mysql');
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "codeup",
+  user: "nodejs",
+  password: "Codeup!1",
   database: 'employees'
 });
 
-db.connect(function(err) {
-  if (err) throw err;
-  console.log("MySQL Connected!");
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("MySQL Connected!");
+  }
 });
 
 // Setup middleware
@@ -26,11 +29,40 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
+//Setup routes
+// Get all employees
+app.get('/api/', (req, res) => {
+  let sql = 'SELECT * FROM empl';
+  let query = db.query(sql, (err, results) => {
+    if(err) throw err;
+    console.log(results);
+    res.json(results);
+  });
+});
 
+// Post using route parameters
+app.post('/api/:name/:age', (req, res) => {
+  let addEmpl = {name: req.params.name, age: req.params.age};
+  let sql = `INSERT INTO empl SET ?`;
+  let query = db.query(sql, addEmpl, (err, results) => {
+    if(err) throw err;
+    res.send(`${addEmpl.name} was added successfully!`);
+  });
+});
 
+// Post using default route and json
+app.post('/api/', (req, res) => {
+  let addEmpl = {name: req.body.name, age: req.body.age};
+  let sql = `INSERT INTO empl SET ?`;
+  let query = db.query(sql, addEmpl, (err, results) => {
+    if(err) throw err;
+    res.send(`${addEmpl.name} was added successfully!`);
+  });
+});
 
-
-
+app.put('/api/:id', (req, res) => {
+  let updateEmpl = {name: req.body.name, age: req.body.age};
+})
 
 
 
